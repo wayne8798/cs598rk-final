1f393276f8eaebf477e37b9c4c740a179eca03d9
haiziyu7 <haiziyu7@illinois.edu>
2014-12-11 11:31:15 -0600

    updates

--- src/ClassShop.py
+++ src/ClassShop.py
@@ -1,10 +1,12 @@
-import cv2
+import os, cv2
 import cv2.cv as cv
 from numpy import *
 
 class ImageKit:
-	def __init__(self, filename):
-		self.img = cv2.imread(filename, cv.CV_LOAD_IMAGE_COLOR)
+	def __init__(self, imgpath):
+		self.imgName = imgpath.split('/')[-1]
+		self.imgDir = imgpath[:-len(self.imgName)]
+		self.img = cv2.imread(imgpath, cv.CV_LOAD_IMAGE_COLOR)
 		if self.img == None:
 			print "Warning: self.img == None!"
 
@@ -25,21 +27,21 @@ class ImageKit:
 
 	def getPixelColor(self, height, weight, threshold): # start from 0
 		pixelVal = int64(self.getPixelValue(height, weight))
-		BLUE = array([255,0,0])
-		GREEN = array([0,255,0])
-		RED = array([0,0,255])
 		BLACK = array([0,0,0])
 		WHITE = array([255,255,255])
-		if max(abs(pixelVal-BLUE)) <= threshold:
-			return 'blue'
-		elif max(abs(pixelVal-GREEN)) <= threshold:
-			return 'green'
-		elif max(abs(pixelVal-RED)) <= threshold:
-			return 'red'
-		elif max(abs(pixelVal-BLACK)) <= threshold:
+		#BLUE = array([255,0,0])
+		#GREEN = array([0,255,0])
+		#RED = array([0,0,255])
+		if max(abs(pixelVal-BLACK)) <= threshold:
 			return 'black'
 		elif max(abs(pixelVal-WHITE)) <= threshold:
 			return 'white'
+		#elif max(abs(pixelVal-BLUE)) <= threshold:
+		#	return 'blue'
+		#elif max(abs(pixelVal-GREEN)) <= threshold:
+		#	return 'green'
+		#elif max(abs(pixelVal-RED)) <= threshold:
+		#	return 'red'
 		else:
 			return 'other'
 
@@ -47,32 +49,28 @@ class ImageKit:
 		count = 0
 		for h in range(self.getHeight()):
 			for w in range(self.getWidth()):
-				if self.getPixelColor(h,w,threshold) == 'black':
+				if not self.getPixelColor(h,w,threshold) == 'black':
 					count += 1
-		blackLevel = float(count)/self.getSize()
-		#print blackLevel
-		if blackLevel >= percentile:
-			return True
-		else:
-			return False
+					if float(count)/self.getSize() > (1-percentile):
+						return False
+		return True
 
 	def isWhite(self, threshold = 20, percentile = 1.0):
 		count = 0
 		for h in range(self.getHeight()):
 			for w in range(self.getWidth()):
-				if self.getPixelColor(h,w,threshold) == 'white':
+				if not self.getPixelColor(h,w,threshold) == 'white':
 					count += 1
-		whiteLevel = float(count)/self.getSize()
-		#print whiteLevel
-		if whiteLevel >= percentile:
-			return True
-		else:
-			return False
+					if float(count)/self.getSize() > (1-percentile):
+						return False
+		return True
 
 
 class VideoKit:
-	def __init__(self, filename):
-		self.vidcap = cv2.VideoCapture(filename)
+	def __init__(self, vidpath):
+		self.vidName = vidpath.split('/')[-1]
+		self.vidDir = vidpath[:-len(self.vidName)]
+		self.vidcap = cv2.VideoCapture(vidpath)
 		if self.vidcap == None:
 			print "Warning: self.vidcap == None!"
 
@@ -93,4 +91,26 @@ class VideoKit:
 
 	def getFrameCount(self):
 		return self.getFPS()*self.getDuration()
+
+	def toImgs(self, nSec=-1):
+		if nSec == -1:
+			outputDir = self.vidDir+'../imgs/'+self.vidName.split('.')[0]+'/'
+			nFrame = 1
+		else:
+			outputDir = self.vidDir+'../imgs/'+self.vidName.split('.')[0]+str(nSec)+'/'
+			nFrame = nSec*self.getFPS()
+		if not os.path.isdir(outputDir):
+			os.mkdir(outputDir)
+		success,image = self.vidcap.read()
+		count = 0
+		while success:
+			if count%nFrame == 0:
+				cv2.imwrite(outputDir+"frame%d.jpg" % (count+1), image)
+			success,image = self.vidcap.read()
+			count += 1
+		self.vidcap = cv2.VideoCapture(self.vidDir+self.vidName)
+		if count == self.getFrameCount():
+			print "Success: video to images completed!"
+		else:
+			print "Failure: frame count error!"
 
--- src/vid2imgs.py
+++ src/vid2imgs.py
@@ -1,28 +1,14 @@
-import sys, os, cv2
+import os, sys
 from ClassShop import *
 
-input_filename = sys.argv[1].split('/')[-1]
-input_dir = sys.argv[1][:-len(input_filename)]
-output_dir = input_dir+'../imgs/'+input_filename.split('.')[0]+'/'
-
-if len(sys.argv)-1 >= 2:
-	nFrame = int(sys.argv[2])
+if len(sys.argv) < 2:
+	print "Error: input video directory not specified!"
 else:
-	nFrame = 1
-
-aVideo = VideoKit(sys.argv[1])
-
-if not os.path.isdir(output_dir):
-	os.mkdir(output_dir)
-success,image = aVideo.vidcap.read()
-countV = 0
-countG = 0
-while success:
-	if countV%nFrame == 0:
-		cv2.imwrite(output_dir+"frame%d.jpg" % (countV+1), image)
-		#cv2.imwrite(output_dir+"frame%d.jpg" % (countG+1), image)
-		countG += 1
-	success,image = aVideo.vidcap.read()
-	countV += 1
-print str(countV)+" frames have been visited!"
-print str(countG)+" frames have been generatd!"
+	inputDir = sys.argv[1]
+	if not inputDir[-1] == '/':
+		inputDir += '/'
+	for vfile in os.listdir(inputDir):
+		if vfile.endswith('.mp4'):
+			currentVid = VideoKit(inputDir+vfile)
+			currentVid.toImgs(nSec=1)
+			currentVid.toImgs(nSec=10)
